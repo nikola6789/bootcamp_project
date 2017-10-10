fea9cec755d06c06818897f148e2c38f515fd09c
Nikola Zivkovic <Nikola.Zivkovic@endava.com>
2017-10-10 10:20:35 +0200

    Initial project structure commit

---
+++ LocationTracker/app/src/main/java/zivkovic/nikola/locationtracker/MyTracks.java
@@ -0,0 +1,13 @@
+package zivkovic.nikola.locationtracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyTracks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_tracks);
+    }
+}
